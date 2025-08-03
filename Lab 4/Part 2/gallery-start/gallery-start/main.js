55c30f601a2d02da0f13fe413aa264e6cdbd03be
Git-Manjeet <manjeet07071212@gmail.com>
2025-08-02 20:11:10 -0400

    Uodate*

---
+++ Lab 4/Part 2/gallery-start/gallery-start/main.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+btn.addEventListener('click', () => {
+  const currentClass = btn.getAttribute('class');
+  if (currentClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
